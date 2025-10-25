8aba3678f4b7d52744d819023671d5cdf58a2933
Emmanuel Diaz <emmanueldiaz707@gmail.com>
2025-10-25 13:10:36 -0300

    implementar menu calculo de edad

--- prueba-pascal-1/src/umenu.pas
+++ prueba-pascal-1/src/umenu.pas
@@ -33,14 +33,14 @@ begin
     repeat
         op := ' ';
         writeLn('Menú principal');
-        writeLn('1 - Opción 1');
+        writeLn('1 - Calcular edad según año de nacimiento');
         writeLn('2 - Opción 2');
         writeLn('3 - Opción 3');
         writeLn('0 - Salir');
         op := readkey;
 
         case op of
-            '1': writeLn('Opción 1');
+            '1': menuEdad;
             '2': writeLn('Opción 2');
             '3': writeLn('Opción 3');
         end;
